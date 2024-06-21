12f3b4c8391441c349172904c9e7977c5aeaeef4
MuffinShades <89941934+MuffinShades@users.noreply.github.com>
2024-06-21 19:49:00 -0400

    added some wrapper code on le api

--- wallgle.api.js
+++ wallgle.api.js
@@ -1,12 +1,29 @@
-(function(api) {
-    if (typeof api == 'function') {
-        api();
-        return;
+(function(api){if(typeof api!='object'){if(typeof api=='function')api();return}
+//write all api code here and export it by doing api.
+
+api = {
+    status: 0,
+    getStatus: function() {
+        return this.status;
     }
-
-    api.test = 1;
-    console.log(api);
+};
 
 
-})(typeof window == 'object' ? window._wapi={} : function() {console.error('Wallgle API failed to load :/')});
-const WallgleApi=window._wapi;console.log(WallgleApi);
+})(typeof window=='object'?window._wapi={}:function(){console.error('Wallgle API failed to load :/')})=window._apiCode;
+switch (typeof window._apiCode) {
+    case 'object': {
+        window.ApiCode = window._apiCode.getStatus();
+        window._wapi = window._apiCode;
+        break;
+    }
+    case 'number': {
+        window.ApiCode = window._apiCode;
+        break;
+    }
+    default: {
+        console.error('Failed to load api, got code ',window._apiCode);
+        window._wapi=void 0;
+        break;
+    }
+}
+const WallgleApi=window._wapi;
