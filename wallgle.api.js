d0bf9ec31c5b4268d7f87da580e3f8e90c124317
MuffinShades <89941934+MuffinShades@users.noreply.github.com>
2024-06-21 20:06:57 -0400

    some basic classes and export function

--- wallgle.api.js
+++ wallgle.api.js
@@ -3,16 +3,64 @@
         if(typeof api=='function')api();
         return
     }
-//write all api code here and export it by doing api.
+//write all api code here and export it by doing api.something =
 
+/*
+
+Main API class
+This is what will be exported, all other classes wont
+
+*/
 api = {
     status: 0,
     getStatus: function() {
         return this.status;
+    },
+    getPublicAccountInfo: function(id) {
+
+    },
+    exportObjectAs: function(c, a) {
+        if (typeof c != typeof void 0 && typeof a == 'string')
+            this[a] = c;
     }
 };
 
+//Communications class
+/*
 
+For communication with main server
+This will be in the api but make it
+secure porfavor
+
+*/
+var Communicator = {
+    Connect: null,
+};
+
+Communicator.Connect = function(url) {
+
+}
+
+//add random things that would be useful here oh yeah Zlib >:)
+const Zlib = {
+    Deflate: null,
+    Inflate: null,
+}
+
+api.exportObjectAs(Zlib, 'Zlib');
+
+const BitStream = {
+    bytes: [],
+    len: 0,
+    cBit: 0,
+}
+
+//
+
+
+
+//remove this before final export cause well we cant have those rapscallions accessing thing function
+api.exportObjectAs=void 0;
 })(typeof window=='object'?window._wapi={}:function(){
     console.error('Wallgle API failed to load :/')
 })=window._apiCode;
